625544a35a779d3bffe0077aba5186b775df6a87
Gabriel Silva <gabriel.silva@siematec.local>
2024-11-01 15:08:11 -0300

    showSectionAdded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-const showCharacters = ()=>{
-    
+const showSection = (sectionId)=>{
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const sectionsShow = document.getElementById(sectionId)
+    sectionsShow.style.display = 'block';
 }
